a485029ec01e54e5ec4e9def670cd47c9ef90ff0
Mykhailo Vorobets <mikevorobets@gmail.com>
2024-09-13 23:44:58 +0300

    added

--- shop/settings.py
+++ shop/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = ['static/']
 
 # White noise static stuff
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
