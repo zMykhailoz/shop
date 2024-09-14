ed828b411a3a4b334829ec181154389972e61ec2
Mykhailo Vorobets <mikevorobets@gmail.com>
2024-09-14 10:34:36 +0300

    added changes

--- payment/views.py
+++ payment/views.py
@@ -8,6 +8,11 @@ from payment.models import ShippingAddress, Order, OrderItem
 from django.contrib import messages
 
 from store.models import Profile
+# Import Some PayPal Stuff
+from django.urls import reverse
+from paypal.standard.forms import PayPalPaymentsForm
+from django.conf import settings
+import uuid
 
 
 def orders(request, pk):
@@ -191,12 +196,29 @@ def billing_info(request):
         my_shipping = request.POST
         request.session['my_shipping'] = my_shipping
 
+        # Get the host
+        host = request.get_host()
+        # Creat PayPal Form and Stuff
+        paypal_dict = {
+            "business": settings.PAYPAL_RECEIEVER_EMAIL,
+            "amount": totals,
+            "item_name": "Order",
+            "no_shipping": "2",
+            "invoice": str(uuid.uuid4()),
+            "currency_code": "EUR",
+            "notify_url": "https://{}{}".format(host, reverse("paypal-ipn")),
+            "return_url": "https://{}{}".format(host, reverse("payment_success")),
+            "cancel_return": "https://{}{}".format(host, reverse("payment_failed")),
+        }
+        # Creat actual paypal button
+        paypal_form = PayPalPaymentsForm(initial=paypal_dict)
+
         # Check to see if user is logged in
         if request.user.is_authenticated:
             # Get The Billing Form
             billing_form = PaymentForm()
             return render(request, "payment/billing_info.html",
-                          {"cart_products": cart_products, "quantities": quantities,
+                          {"paypal_form": paypal_form, "cart_products": cart_products, "quantities": quantities,
                            "totals": totals, "shipping_info": request.POST, "billing_form": billing_form})
 
         else:
@@ -204,12 +226,12 @@ def billing_info(request):
             # Get The Billing Form
             billing_form = PaymentForm()
             return render(request, "payment/billing_info.html",
-                          {"cart_products": cart_products, "quantities": quantities,
+                          {"paypal_form": paypal_form, "cart_products": cart_products, "quantities": quantities,
                            "totals": totals, "shipping_info": request.POST, "billing_form": billing_form})
 
         shipping_form = request.POST
         return render(request, "payment/billing_info.html",
-                      {"cart_products": cart_products, "quantities": quantities, "totals": totals,
+                      {"paypal_form": paypal_form, "cart_products": cart_products, "quantities": quantities, "totals": totals,
                        "shipping_form": shipping_form})
     else:
         messages.success(request, "Access Denied")
@@ -241,3 +263,7 @@ def checkout(request):
 
 def payment_success(request):
     return render(request, 'payment/payment_success.html', {})
+
+
+def payment_failed(request):
+    return render(request, 'payment/payment_failed.html', {})
--- shop/settings.py
+++ shop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'cart',
     'payment',
     'whitenoise.runserver_nostatic',
+    'paypal.standard.ipn',
 
 ]
 
@@ -143,3 +144,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Add paypal settings
+PAYPAL_TEST = True
+
+PAYPAL_RECEIVER_EMAIL = 'mishaforever@bigmir.net'
